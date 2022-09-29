df5a2407e9a7a1587be8e41521fd949725e57d8c
VladimirSteshin <warheim24@gmail.com>
2022-09-29 20:39:26 +0300

    Model

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,10 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50, verbose_name='Модель телефона')
+    price = models.IntegerField(verbose_name='Цена')
+    image = models.ImageField(upload_to='', verbose_name='Изображение')
+    release_date = models.DateField(verbose_name='Дата выпуска')
+    lte_exists = models.BooleanField
+    slug = models.SlugField(name)
