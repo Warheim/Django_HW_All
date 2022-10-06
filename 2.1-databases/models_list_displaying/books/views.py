25fab092b49f26457912dab3ad221f890c4e6d69
Steshin <warheim24@gmail.com>
2022-10-06 11:20:49 +0300

    books loaded, dates are working

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,25 @@
+from django.core.exceptions import ValidationError
 from django.shortcuts import render
+from .models import Book
+from django.http import HttpResponse
 
 
 def books_view(request):
+    books = Book.objects.all().order_by('pub_date')
     template = 'books/books_list.html'
-    context = {}
+    context = {'books': books}
     return render(request, template, context)
+
+
+def books_for_date(request, pub_date):
+    try:
+        books = Book.objects.filter(pub_date=pub_date)
+        if books:
+            template = 'books/books_list.html'
+            context = {'books': books}
+            return render(request, template, context)
+        else:
+            return HttpResponse('<h3>No books for such a date</h3>')
+    except ValidationError:
+        return HttpResponse(
+            f'<h3>"{pub_date}" is incorrect format for date. Please type date in YYYY-MM-DD format.</h3>')
