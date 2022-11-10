c375e59d00040ccfdf1e719c687ac91632c49d67
Steshin <warheim24@gmail.com>
2022-11-10 16:57:51 +0300

    DateFilter added

--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,5 +1,7 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
+
+from .filters import AdvertisementFilter
 from .models import Advertisement
 from .permissions import IsOwner
 from .serializers import AdvertisementSerializer
@@ -11,10 +13,10 @@ class AdvertisementViewSet(ModelViewSet):
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['creator', 'created_at', 'status']
+    filterset_class = AdvertisementFilter
 
     def get_permissions(self):
         """Получение прав для действий."""
-        if self.action in ["create", "update", "partial_update"]:
+        if self.action in ["create", "update", "partial_update", "destroy"]:
             return [IsOwner()]
         return []
